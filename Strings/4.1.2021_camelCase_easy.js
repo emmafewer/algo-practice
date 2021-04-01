b1bb438acca87d2527134cfa52fe34189283f2e8
emmafewer <emmafewer20@gmail.com>
2021-04-01 11:01:11 -0700

    camelCase string algo codewars

---
+++ Strings/4.1.2021_camelCase_easy.js
@@ -0,0 +1,9 @@
+// Strings
+// 6 kyu Codewars
+// https://www.codewars.com/kata/587731fda577b3d1b0001196
+
+String.prototype.camelCase=function(){
+  return this.split(" ").map(word => word[0].toUpperCase()+word.slice(1)).join("")
+}
+
+"this word".camelCase()
